dbef3c6754f169c501004a4555b3ad674a59404d
Roshan Prabhakar <roshancvp@gmail.com>
2016-11-09 20:23:30 -0800

    Handlebars and forms

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', {id: req.params.id});
+});
+
+router.post('/test/submit', function(req, res, next) {
+  var id = req.body.id;
+  res.redirect('/test/' + id);
+});
+
 module.exports = router;
